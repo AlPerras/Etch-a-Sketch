c25f6b56797dd88b9044634c900cd8d980df851a
Alexandre Perras <alexperras74@gmail.com>
2024-12-04 21:28:45 +0800

    added the hover effet that leave a pixalated trail

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector("#container");
 
+function getRandomRGBColor() {
+  const r = Math.floor(Math.random() * 256); // Random value between 0 and 255
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 for (let i = 0; i < 256; i++) {
     const squarre = document.createElement("div");
     squarre.id = "grid";
+    // squarre.className = [i];
     container.appendChild(squarre);
-    console.log(i);
-}
+    squarre.addEventListener("mouseenter", () => {
+        squarre.style.transition = "0.3ms";
+        squarre.style.backgroundColor = getRandomRGBColor();
+    });
+};
